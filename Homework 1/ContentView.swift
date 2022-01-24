e93f3386e1f7fa1a566f778b82f34a0ccabd261a
Matthew Malaker <MMalaker@hawk.iit.edu>
2022-01-24 13:10:17 -0600

    Initial Commit

---
+++ Homework 1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Homework 1
+//
+//  Created by Matthew Malaker on 1/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Homework 1/Homework_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework_1App.swift
+//  Homework 1
+//
+//  Created by Matthew Malaker on 1/24/22.
+//
+
+import SwiftUI
+
+@main
+struct Homework_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
